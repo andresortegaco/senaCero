9c610d489b08caf9964eb0170c7224e0f52afd41
ananorco <andresortegacoello@gmail.com>
2025-11-06 21:25:39 -0500

    Se agrego el borrado de los repetidos

--- src/ejemploconexionjdbc/EjemploConexionJDBC.java
+++ src/ejemploconexionjdbc/EjemploConexionJDBC.java
@@ -34,7 +34,7 @@ public class EjemploConexionJDBC {
                 System.getLogger(EjemploConexionJDBC.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
             }
             
-            
+            System.out.println("Aqui insertamos un nuevo cargo");
             conexion = DriverManager.getConnection(url,usuario,password);
             statement = conexion.createStatement();
             statement.executeUpdate("INSERT INTO CARGOS(nombre_cargo,salario) VALUES('Vigilante','1700000')");
@@ -44,6 +44,21 @@ public class EjemploConexionJDBC {
                 System.out.println(rs.getInt("id_cargo")+ " : " + rs.getString("nombre_cargo")+" : " + rs.getString("salario"));
             }while (rs.next());
             
+            System.out.println("Aqui borramos el cargo insertado previamente");
+            
+            statement.executeUpdate("DELETE t1\n" +
+            "FROM \n" +
+            "    cargos t1\n" +
+            "INNER JOIN \n" +
+            "    cargos t2 \n" +
+            "WHERE \n" +
+            "    t1.nombre_cargo = t2.nombre_cargo  " +
+            "    AND t1.id_cargo > t2.id_cargo;");
+            rs = statement.executeQuery ("SELECT * FROM CARGOS");
+            rs.next();
+            do{
+                System.out.println(rs.getInt("id_cargo")+ " : " + rs.getString("nombre_cargo")+" : " + rs.getString("salario"));
+            }while (rs.next());
             
             
         } catch (SQLException ex) {
